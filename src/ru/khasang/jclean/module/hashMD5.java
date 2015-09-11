42e318457f7394617a2f2d8d4eee7716e7f654a1
Владислав <vlados_corp@yahoo.com>
2015-09-12 01:06:48 +0300

    сделал класс HashMD5, содержащий методы для выдачи хеш-суммы

--- src/ru/khasang/jclean/module/hashMD5.java
+++ src/ru/khasang/jclean/module/hashMD5.java
@@ -1,7 +1,40 @@
 package ru.khasang.jclean.module;
 
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.security.MessageDigest;
+
 /**
  * Created by GamBit on 12/09/15.
  */
-public class hashMD5 {
+public class HashMD5 {
+
+    public static byte[] createChecksum(String filename) throws Exception {
+        InputStream fis = new FileInputStream(filename);
+
+        byte[] buffer = new byte[1024];
+        MessageDigest complete = MessageDigest.getInstance("MD5");
+        int numRead;
+
+        do {
+            numRead = fis.read(buffer);
+            if (numRead > 0) {
+                complete.update(buffer, 0, numRead);
+            }
+        } while (numRead != -1);
+
+        fis.close();
+        return complete.digest();
+    }
+
+    //возвращает хеш-МД5 в виде строки
+    public static String getMD5Checksum(String filename) throws Exception {
+        byte[] b = createChecksum(filename);
+        String result = "";
+
+        for (int i = 0; i < b.length; i++) {
+            result += Integer.toString((b[i] & 0xff) + 0x100, 16).substring(1);
+        }
+        return result;
+    }
 }
