1e4087b5d93b7655efcd8616f4f0a9967a5022c5
Владислав <vlados_corp@yahoo.com>
2015-09-12 00:41:26 +0300

    мелкие изменения JCFileProperty

--- src/ru/khasang/jclean/module/JCFileProperty.java
+++ src/ru/khasang/jclean/module/JCFileProperty.java
@@ -5,18 +5,18 @@ import java.util.ArrayList;
 /**
  * Created by GamBit on 12/09/15.
  */
-public class JCFilePropertie {
+public class JCFileProperty {
     //имя первого найденного дубликата
     private String name;
 
     //адреса всех файлов с одинаковыми размерами
-    public ArrayList<String> copies = new ArrayList<String>();
+    public ArrayList<String> copies = new ArrayList<>();
 
     public String getName() {
         return name;
     }
 
-    public JCFilePropertie(String name, String absolutePath, String absolutePath2) {
+    public JCFileProperty(String name, String absolutePath, String absolutePath2) {
         this.name = name;
         this.copies.add(absolutePath);
         this.copies.add(absolutePath2);
