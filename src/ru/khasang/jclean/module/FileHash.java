d786c28f2d165fd24d2f6f8abaa76f9fe544cd9e
Владислав <vlados_corp@yahoo.com>
2015-09-13 21:49:58 +0300

    FileHash принимает строку с адресом

--- src/ru/khasang/jclean/module/FileHash.java
+++ src/ru/khasang/jclean/module/FileHash.java
@@ -2,7 +2,7 @@ package ru.khasang.jclean.module;
 
 public class FileHash {
 
-    public static String getFastHash() {
+    public static String getFastHash(String path) {
         return null;
     }
 }
